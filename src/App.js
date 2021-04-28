6f6c19fe79723e27130dc1135706cdcd3fb31163
santosh <khadkasantosh009@gmail.com>
2021-04-28 17:57:05 +0545

    you can now add a task

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import {useState } from 'react'
 import Header from './components/Header'
 import AvailableTask from './components/AvailableTask'
+import AddTask from './components/AddTask'
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
     {
         id: 1, 
@@ -24,16 +26,26 @@ function App() {
     }
 ])
 
+//Add Task
+const addTask = (task) => {
+  const id = Math.floor(Math.random() * 1000) + 1
+  const newTask = {id, ...task}
+  setTasks([...tasks, newTask])
+}
+
+// Delete Task
 const taskDelete = (id) => {
   setTasks(tasks.filter((task)=>task.id !== id))
 }
 
+// Toggle task
 const toggleReminder = (id) => {
   setTasks(tasks.map((task)=>task.id === id ? {...task, reminder: !task.reminder}: task))
 }
   return (
     <div className="App">
-      <Header />
+      <Header onAdd={()=>setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+      {showAddTask && <AddTask onAdd={addTask}/> }
       {tasks.length > 0 ? <AvailableTask tasks={tasks} onDelete={taskDelete} onToggle={toggleReminder}/> 
       : 'No Task to Show'}
     </div>
