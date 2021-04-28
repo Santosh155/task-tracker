0ac6f046e0bee12897a7593369dacfe831e10ba3
santosh <khadkasantosh009@gmail.com>
2021-04-28 16:32:32 +0545

    task seprated and made more easy

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
+import {useState } from 'react'
 import Header from './components/Header'
-import Tasks from './components/Tasks'
+import AvailableTask from './components/AvailableTask'
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1, 
+        text: 'Doctor Appointment',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true
+    },
+    {
+        id: 2, 
+        text: 'School Appointment',
+        day: 'Feb 6th at 1:30pm',
+        reminder: true
+    },
+    {
+        id: 3, 
+        text: 'Food Shopping',
+        day: 'Feb 5th at 1:30pm',
+        reminder: false
+    }
+])
   return (
     <div className="App">
       <Header />
-      <Tasks />
+      <AvailableTask tasks={tasks}/>
     </div>
   );
 }
