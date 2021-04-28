29695570e17832dee134707de92d476f21b084b8
santosh <khadkasantosh009@gmail.com>
2021-04-28 23:50:16 +0545

    route modified and some filogics fixed

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,10 @@
+import {Link} from 'react-router-dom'
+
 const About = () => {
     return (
         <div>
             <p>Version 1.0.0</p>
-            <a href="/">Go back</a>
+            <Link to="/">Go back</Link>
         </div>
     )
 }
